3ba5cdf6c8b6de29b50dbc783d614e6a71cdca8b
viper75 <sean.huvaya03@yahoo.com>
2020-08-20 15:26:49 +0200

    Initial Commit

---
+++ app/src/main/java/com/codewithsean/android_web_socket_client/MainActivity.java
@@ -0,0 +1,99 @@
+package com.codewithsean.android_web_socket_client;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import tech.gusavila92.websocketclient.WebSocketClient;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private WebSocketClient webSocketClient;
+    private TextView resultTextView;
+    private Button sendMessageBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+        createWebSocketClient();
+    }
+
+    private void initViews() {
+        resultTextView = findViewById(R.id.result_tv);
+        sendMessageBtn = findViewById(R.id.send_msg_btn);
+
+        sendMessageBtn.setOnClickListener(this);
+    }
+
+    private void createWebSocketClient() {
+        URI uri;
+        try {
+            // Connect to local host
+            uri = new URI("wss://10.0.2.2:8080/websocket");
+        }
+        catch (URISyntaxException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        webSocketClient = new WebSocketClient(uri) {
+            @Override
+            public void onOpen() {
+                Log.i("WebSocket", "Session is starting");
+                webSocketClient.send("Hello World!");
+            }
+
+            @Override
+            public void onTextReceived(String s) {
+                Log.i("WebSocket", "Message received");
+                final String message = s;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try{
+                            resultTextView.setText(message);
+                        } catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+            @Override
+            public void onBinaryReceived(byte[] data) {
+            }
+            @Override
+            public void onPingReceived(byte[] data) {
+            }
+            @Override
+            public void onPongReceived(byte[] data) {
+            }
+            @Override
+            public void onException(Exception e) {
+                System.out.println(e.getMessage());
+            }
+            @Override
+            public void onCloseReceived() {
+                Log.i("WebSocket", "Closed ");
+                System.out.println("onCloseReceived");
+            }
+        };
+        webSocketClient.setConnectTimeout(10000);
+        webSocketClient.setReadTimeout(60000);
+        webSocketClient.enableAutomaticReconnection(5000);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.send_msg_btn) {
+            webSocketClient.send("From android device");
+        }
+    }
+}
